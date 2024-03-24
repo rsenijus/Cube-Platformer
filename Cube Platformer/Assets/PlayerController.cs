b476b34ac343c45246f58c5ae4a417487bc323ad
rsenijus <suhinovarsenij@gmail.com>
2024-03-24 10:03:17 +0100

    Player Controller v0.1

---
+++ Cube Platformer/Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    public float speed = 1.0f;
+    private Vector3 moveVector;
+    public bool isGrounded;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveVector.z = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveVector.z = -1;
+        }
+        else
+        {
+            moveVector.z = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveVector.x = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveVector.x = -1;
+        }
+        else
+        {
+            moveVector.x = 0;
+        }
+        rb.MovePosition(rb.position + moveVector * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+}
