769d0d97dca60680b26e67f9ce12ddfd5748ed97
rsenijus <suhinovarsenij@gmail.com>
2024-03-28 19:40:34 +0100

    Player Controller v0.2

--- Cube Platformer/Assets/PlayerController.cs
+++ Cube Platformer/Assets/PlayerController.cs
@@ -5,58 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    public float speed = 1.0f;
+    [SerializeField] public float speed = 1.0f;
     private Vector3 moveVector;
-    public bool isGrounded;
-    public Vector3 jump;
-    public float jumpForce = 5.0f;
+    private bool isGrounded;
+    private Vector3 jump;
+    [SerializeField] public float jumpForce = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        jump = new Vector3(0.0f, 1.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            moveVector.z = 1;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            moveVector.z = -1;
-        }
-        else
-        {
-            moveVector.z = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            moveVector.x = 1;
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            moveVector.x = -1;
-        }
-        else
-        {
-            moveVector.x = 0;
-        }
-        rb.MovePosition(rb.position + moveVector * speed);
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.z = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
 
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
         }
     }
 
     void OnCollisionStay()
     {
         isGrounded = true;
+    }
+
+    private void OnCollisionExit()
+    {
+        isGrounded = false;
     }
 }
